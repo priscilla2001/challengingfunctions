b721e57a3dae8582718729f0997e57f27a31d1e3
priscilla2001 <71602539+priscilla2001@users.noreply.github.com>
2020-09-24 16:02:45 +0530

    Add files via upload

---
+++ countingpostive.py
@@ -0,0 +1,11 @@
+a=[]
+for i in range(0,5,1):
+    print("enter a[%d]"%i)
+    new=int(input())
+    a.append(new)
+print(a)
+print("The number of positive values")
+for list in a:
+  if list>=1:
+    print(list)
+https://github.com/priscilla2001/countingpositiveno-s.git
